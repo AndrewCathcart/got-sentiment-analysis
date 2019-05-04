2d9e516a06a9ad51942cee4f1cc7d0a5c0bc39da
andrewcathcart <andrew.cathcart@salecycle.com>
2019-05-04 01:49:45 +0100

    filter retweets out - unneeded bloat

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 with open('gameofthrones.csv', 'a') as file:
     writer = csv.writer(file)
     for tweet in tweepy.Cursor(
-            api.search, tweet_mode='extended', q="#gameofthrones", count=200, lang="en", since="2019-04-27").items():
-        print(tweet.created_at, tweet.text)
+            api.search, tweet_mode='extended', q="#gameofthrones -filter:retweets", count=200, lang="en",
+            since="2019-04-27").items():
+        print(tweet.created_at, tweet.full_text)
         writer.writerow([tweet.created_at, tweet.full_text.encode('utf-8')])
-        writer.close()
+    writer.close()
